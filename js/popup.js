ba2bb36eddb4638a20696b48e3c661f84472839f
Gurpreet Singh <gurpreet@Gurpreets-MacBook-Pro.local>
2017-02-03 22:55:48 -0800

    testing tabs api: works

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,30 @@
 
 
-function test(){
-console.log("sup");
+var d = document,
+createTask = d.getElementById('createTask');
+
+
+
+createTask.addEventListener('click', createNewTab);
+	
+
+function createNewTab()
+{
+var createProperties = {
+    active: true,
+    index: 1,
+    url: "https://www.google.com/"
+  };
+	chrome.tabs.create(createProperties, function(response)
+	{
+		if(response)
+		{
+			console.log("new window created");
+		}
+	});
 }
+
+
+
+
+
