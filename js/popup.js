44631df0170f2a701380fa7b5005968ef314beb3
Gurpreet Singh <gurpreet@Gurpreets-MacBook-Pro.local>
2017-02-04 16:55:23 -0800

    cleaning up

--- js/popup.js
+++ js/popup.js
@@ -8,17 +8,37 @@ createTask = d.getElementById('createTask');
 createTask.addEventListener('click', createNewTab);
 	
 
+var name = "time to pay bill";
+
+var alarmInfo = {
+  when: Date.now() + 1000
+  };
+
+
+function setAlarm() {
+	// body...
+	alert("setting up alarm");
+	chrome.alarms.create(name, alarmInfo);
+	alert("alarm went off")
+
+}
+
+
+
+
 function createNewTab()
 {
 var createProperties = {
     active: true,
     index: 1,
-    url: "https://www.google.com/"
+    //url: "https://www.google.com/",
+    url: "http://singhgurpreet.us/"
   };
 	chrome.tabs.create(createProperties, function(response)
 	{
 		if(response)
 		{
+			setAlarm();
 			console.log("new window created");
 		}
 	});
