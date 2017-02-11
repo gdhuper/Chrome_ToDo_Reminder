cc5f01b41fe1e38add8cd4c0ec39b5120dc3f2c8
Gurpreet Singh <gurpreet@Gurpreets-MacBook-Pro.local>
2017-02-10 21:44:58 -0800

    Testing btn dropdown

--- js/popup.js
+++ js/popup.js
@@ -1,46 +1,87 @@
+var popup = function(){
 
-var d = document,
-createTask = d.getElementById('createTask');
-calIcon = d.getElementById('calIcon');
 
+	//var ds = require('datastructures-js');
+	//var pQueue = ds.priorityQueue();
+
+	/*Index page variables*/
+	var index = {
+		idxHeading: $('#topTitle'),
+		createTask: $('#createTask'),
+		addTask: $('#addTask'),
+		front: $('#frontPanel'),
+		cancel: $('#cancelSave'),
+		calIcon: $('#calIcon'),
+	};
+
+
+
+//cancel.addEventListener('click', cancelSaving);
 calIcon.addEventListener('click', popCal);
-function popCal()
+createTask.addEventListener('click', addTaskToList);
+
+function cancelSaving()
 {
-	console.log("picking time");
-     $('#dueDateTime').datetimepicker({
-    format: 'YYYY/MM/DD hh:mm A'
-  });
+	console.log("callign cancelSaving")
+	$('#frontPanel').remove('hidden');
+	$('#topTitle').remove('hidden');
+
+	$('#addTask').addClass('hidden');
+
+
 }
 
-createTask.addEventListener('click', createNewTab);
+function addTaskToList()
+{
+	console.log("calling create task");
+	$('#frontPanel').addClass('hidden');
+	$('#topTitle').addClass('hidden');
+
+	$('#addTask').removeClass('hidden');
+
+
+}
+
+
+
+	//$('#calIcon').addEventListener('click', popCal);
+	function popCal()
+	{
+		console.log("picking time");
+		$('#dueDateTime').datetimepicker({
+			format: 'YYYY/MM/DD hh:mm A'
+		});
+	}
+
+	//createTask.addEventListener('click', createNewTab);
+
+
+	var name = "time to pay bill";
+
+	var alarmInfo = {
+		when: Date.now() + 1000
+	};
+
+
+	function setAlarm() {
+		// body...
+		alert("setting up alarm");
+		chrome.alarms.create(name, alarmInfo);
+		alert("alarm went off")
+
+	}
+
 	
 
-var name = "time to pay bill";
 
-var alarmInfo = {
-  when: Date.now() + 1000
-  };
-
-
-function setAlarm() {
-	// body...
-	alert("setting up alarm");
-	chrome.alarms.create(name, alarmInfo);
-	alert("alarm went off")
-
-}
-
-
-
-
-function createNewTab()
-{
-var createProperties = {
-    active: true,
-    index: 1,
-    //url: "https://www.google.com/",
-    url: "http://singhgurpreet.us/"
-  };
+	function createNewTab()
+	{
+		var createProperties = {
+			active: true,
+			index: 1,
+	    //url: "https://www.google.com/",
+	    url: "http://singhgurpreet.us/"
+	};
 	chrome.tabs.create(createProperties, function(response)
 	{
 		if(response)
@@ -53,5 +94,19 @@ var createProperties = {
 
 
 
+	//
+	$('.dropdown-menu').on('click', 'a', function() {
+		console.log("changing button text");
+
+		var text = $(this).html();
+		var option = text + ' <span class="caret"></span>';
+		$(this).closest('.btn-group').find('.dropdown-toggle').html(option);
 
 
+	});
+
+}
+
+
+var pop =  new popup();
+pop();
